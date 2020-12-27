8be55634f885c27d68afffa4af9f35fcec485d48
Bernard <46226572+bernardphua@users.noreply.github.com>
2020-12-27 23:52:21 +0800

    Add project files.

---
+++ Models/Tetris/Piece.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorGames.Models.Tetris
+{
+    public class Piece
+    {
+        public int[][,] TetrominoPiece { get; set; }
+        public string Color { get; set; }
+        public int TetrominoPieceN { get;  set; }
+        public int[,] ActiveTetromino { get; set; }
+        public int x {get; set;}
+        public int y {get; set;}
+
+        public Piece(int[][,] tetromino, string color)
+        {
+            TetrominoPiece = tetromino;
+            Color = color;
+
+            TetrominoPieceN = 0;
+            ActiveTetromino = TetrominoPiece[TetrominoPieceN];
+
+            x = 3;
+            y = -2;
+        }
+    }
+}
---
+++ Models/Tetris/Tetromino.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorGames.Models.Tetris
+{
+    public class Tetromino
+    {
+        public readonly int[][,] I = new int[4][,]
+        {
+            new int[,] {
+                            {0, 0, 0, 0},
+                            {1, 1, 1, 1},
+                            {0, 0, 0, 0},
+                            {0, 0, 0, 0}
+                       },
+            new int[,] {
+                            { 0, 0, 1, 0},
+                            { 0, 0, 1, 0},
+                            { 0, 0, 1, 0},
+                            { 0, 0, 1, 0}
+                       },
+            new int[,] {
+                            { 0, 0, 0, 0},
+                            { 0, 0, 0, 0},
+                            { 1, 1, 1, 1},
+                            { 0, 0, 0, 0}
+                       },
+            new int[,]{
+                            { 0, 1, 0, 0},
+                            { 0, 1, 0, 0},
+                            { 0, 1, 0, 0},
+                            { 0, 1, 0, 0}
+                      }
+        };
+        
+        public readonly int[][,] J = new int[4][,]
+        {
+            new int[,] {
+                            { 1, 0, 0},
+                            { 1, 1, 1},
+                            { 0, 0, 0}
+                       },
+            new int[,] {
+                            { 0, 1, 1 },
+                            { 0, 1, 0 },
+                            { 0, 1, 0 }
+                       },
+            new int[,] {
+                            { 0, 0, 0 },
+                            { 1, 1, 1 },
+                            { 0, 0, 1 }
+                       },
+            new int[,]{
+                            { 0, 1, 0 },
+                            { 0, 1, 0 },
+                            { 1, 1, 0 }
+                      }
+        };
+        
+        public readonly int[][,] L = new int[4][,]
+        {
+            new int[,] {
+                            { 0, 0, 1},
+                            { 1, 1, 1},
+                            { 0, 0, 0}
+                   },
+            new int[,] {
+                            { 0, 1, 0 },
+                            { 0, 1, 0 },
+                            { 0, 1, 1 }
+                   },
+            new int[,] {
+                            { 0, 0, 0 },
+                            { 1, 1, 1 },
+                            { 1, 0, 0 }
+                   },
+            new int[,] {
+                            { 1, 1, 0 },
+                            { 0, 1, 0 },
+                            { 0, 1, 0 }
+                       }
+        };
+        
+        public readonly int[][,] O = new int[1][,]
+        {
+            new int[,] {
+                            { 0, 0, 0, 0 },
+                            { 0, 1, 1, 0 },
+                            { 0, 1, 1, 0 },
+                            { 0, 0, 0, 0 }
+                       }
+        };
+        
+        public readonly int[][,] S = new int[4][,]
+        {
+            new int[,] {
+                            { 0, 1, 1},
+                            { 1, 1, 0},
+                            { 0, 0, 0}
+                   },
+            new int[,] {
+                            { 0, 1, 0 },
+                            { 0, 1, 1 },
+                            { 0, 0, 1 }
+                   },
+            new int[,] {
+                            { 0, 0, 0 },
+                            { 0, 1, 1 },
+                            { 1, 1, 0 }
+                   },
+            new int[,] {
+                            { 1, 0, 0 },
+                            { 1, 1, 0 },
+                            { 0, 1, 0 }
+                       }
+        };
+        
+        public readonly int[][,] T = new int[4][,]
+        {
+            new int[,] {
+                            { 0, 1, 0},
+                            { 1, 1, 1},
+                            { 0, 0, 0}
+                   },
+            new int[,] {
+                            { 0, 1, 0 },
+                            { 0, 1, 1 },
+                            { 0, 1, 0 }
+                   },
+            new int[,] {
+                            { 0, 0, 0 },
+                            { 1, 1, 1 },
+                            { 0, 1, 0 }
+                   },
+            new int[,] {
+                            { 0, 1, 0 },
+                            { 1, 1, 0 },
+                            { 0, 1, 0 }
+                       }
+        };
+        
+        public readonly int[][,] Z = new int[4][,]
+        {
+            new int[,] {
+                            { 1, 1, 0},
+                            { 0, 1, 1},
+                            { 0, 0, 0}
+                   },
+            new int[,] {
+                            { 0, 0, 1 },
+                            { 0, 1, 1 },
+                            { 0, 1, 0 }
+                   },
+            new int[,] {
+                            { 0, 0, 0 },
+                            { 1, 1, 0 },
+                            { 0, 1, 1 }
+                   },
+            new int[,] {
+                            { 0, 1, 0 },
+                            { 1, 1, 0 },
+                            { 1, 0, 0 }
+                       }
+        };
+
+       
+
+        public Tetromino()
+        {
+           
+        }
+
+    }
+}
+
